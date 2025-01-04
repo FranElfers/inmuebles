c739b548e41601a714dd38f731f4937daaad82e1
Francisco Elfers <fransozzan@gmail.com>
2025-01-04 00:46:44 -0300

    agrego documentacion

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
+## Levantar ambiente de desarrollo
+
 ```bash
 git clone https://github.com/franelfers/inmuebles
 cd inmuebles
 npm install
+```
+Crear el archivo `.env` en la ruta **root** y colocar el siguiente contenido completando los campos:
+```properties
+VITE_API_KEY=
+VITE_PROJECT_ID=
+VITE_MESSAGING_SENDER_ID=
+VITE_APP_ID=
+```
+Hostear proyecto con el comando:
+```bash
 npm run dev
 ```
--- src/tools/atoms.ts
+++ src/tools/atoms.ts
@@ -1,4 +1,23 @@
 import { atom } from 'jotai'
 import { Place } from '../types'
 
+/**
+ * ### Uso de los atoms.
+ * 
+ * El atom `PrimitiveAtom<any>` se puede usar como parametro 
+ * en los hooks `useAtom`, `useAtomValue` y `useSetAtom`. 
+ * El hook `useAtom` funciona como un hook `useState`, 
+ * devolviendo un getter y un setter. Los hooks `useAtomValue` 
+ * y `useSetAtom` funcionan como getter y como setter 
+ * respectivamente, siendo mas eficiente si vamos a usar uno 
+ * solo.
+ * 
+ * @example
+ * import { atom, useAtom, useAtomValue, useSetAtom } from "jotai"
+ * const numberAtom = atom<number>(0)
+ * const [num, setNum] = useAtom(numberAtom)
+ * const getNum = useAtomValue(numberAtom)
+ * const setNum = useSetAtom(numberAtom)
+ */
+
 export const placesAtom = atom<Place[]>([])
