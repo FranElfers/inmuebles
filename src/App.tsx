1cca14d910eafb5588c629165fcf343af09812e4
Francisco Elfers <fransozzan@gmail.com>
2025-01-04 21:00:58 -0300

    Agrego un log para cambio de url

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router'
 import './App.css'
 import MainGrid from './views/Start'
 import Nav from './components/Nav'
 import HomeView from './views/HomeView'
 import PlacesView from './views/PlacesView'
 import PlaceView from './views/PlaceView'
+import React from 'react'
+
+const LogLocation: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const location = useLocation()
+
+  React.useEffect(() => {
+    console.log('[url]', location.pathname)
+  }, [location])
+
+  return <>{children}</>
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Nav />}>
-          <Route index element={<HomeView />} />
-          <Route path="places" element={<PlacesView />} />
-          <Route path="place/:place" element={<PlaceView />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-        <Route path='admin' element={<MainGrid />} />
-      </Routes>
+      <LogLocation>
+        <Routes>
+          <Route element={<Nav />}>
+            <Route index element={<HomeView />} />
+            <Route path="places" element={<PlacesView />} />
+            <Route path="place/:place" element={<PlaceView />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+          <Route path='admin' element={<MainGrid />} />
+        </Routes>
+      </LogLocation>
       {/* <UsersTable />
       <UploadUser /> */}
       {/* <PlacesTable /> */}
